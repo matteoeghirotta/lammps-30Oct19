4359a2417eb1517afde923265da197f15783146d
Matteo Ricci <matteo.ricci@studiomado.it>
2021-03-18 14:48:39 +0100

    fix: src/.gitignore did not include many files

---
+++ src/lmpinstalledpkgs.h
@@ -0,0 +1,10 @@
+#ifndef LMP_INSTALLED_PKGS_H
+#define LMP_INSTALLED_PKGS_H
+const char * LAMMPS_NS::LAMMPS::installed_packages[] = {
+"ASPHERE",
+"KSPACE",
+"MC",
+"MOLECULE",
+"RIGID",
+ NULL };
+#endif
---
+++ src/pair_gayberne.h
@@ -0,0 +1,1 @@
+USER-MOLC/pair_gayberne.h
